fb37a53ceea55cb43864dc888caeb77fc3cdc2e2
Hans Fredrik Maast <hfmaast@gmail.com>
2024-08-22 16:58:35 +0200

    refactor: add props indicating items to change

--- components/GridSwitcher.vue
+++ components/GridSwitcher.vue
@@ -5,9 +5,11 @@ const props = withDefaults(defineProps<{
   items: string[]
   shuffleSpeedInMs?: number
   displayNumberOfItems?: number
+  changeNumberOfItems?: number
 }>(), {
   shuffleSpeedInMs: 3000,
   displayNumberOfItems: 9,
+  changeNumberOfItems: 4,
 })
 
 const displayedItems = ref<string[]>([])
@@ -17,10 +19,9 @@ const getRandomIndex = (arr: string[]) => Math.floor(Math.random() * arr.length)
 const getRandomItems = () => pickRandomItemsFromArray(props.items, props.displayNumberOfItems)
 
 function replaceItemAt(index: number) {
-  let newItem = props.items[index]
-  while (displayedItems.value.includes(newItem)) {
+  let newItem = props.items[getRandomIndex(props.items)]
+  while (displayedItems.value.includes(newItem))
     newItem = props.items[getRandomIndex(props.items)]
-  }
   displayedItems.value[index] = newItem
 }
 
@@ -29,17 +30,19 @@ onMounted(() => {
 
   const usedIndexes = new Set<number>()
 
-  const interval = setInterval(() => {
+  const updateItem = () => {
     let randomIndex = getRandomIndex(displayedItems.value)
-
     if (usedIndexes.size === displayedItems.value.length)
       usedIndexes.clear()
-
     while (usedIndexes.has(randomIndex))
       randomIndex = getRandomIndex(displayedItems.value)
-
     usedIndexes.add(randomIndex)
     replaceItemAt(randomIndex)
+  }
+
+  const interval = setInterval(() => {
+    for (let i = 0; i < props.changeNumberOfItems; i++)
+      updateItem()
   }, props.shuffleSpeedInMs)
 
   onUnmounted(() => clearInterval(interval))
